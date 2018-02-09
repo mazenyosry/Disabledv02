5b1722cf424ed8a89bba4b2bed103b5d13876416
mazenyosry <mizo_yosry@hotmail.com>
2018-02-09 17:07:39 +0200

    -Added sex class -added sex xml -added implementation to the views in the build gradle - making intent from the signup activity

--- app/src/main/java/com/example/application/disabledv01/Sex.java
+++ app/src/main/java/com/example/application/disabledv01/Sex.java
@@ -1,11 +1,13 @@
 package com.example.application.disabledv01;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 import io.rmiri.buttonloading.ButtonLoading;
 
-public class Gender extends AppCompatActivity {
+public class Sex extends AppCompatActivity {
 
     ButtonLoading buttonmale, buttonfemale;
 
@@ -35,17 +37,26 @@ public class Gender extends AppCompatActivity {
         buttonmale.setOnButtonLoadingListener(new ButtonLoading.OnButtonLoadingListener() {
             @Override
             public void onClick() {
-                //...
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        buttonmale.setProgress(false);
+                    }
+                }, 700);
             }
 
             @Override
             public void onStart() {
                 //...
+
             }
 
             @Override
             public void onFinish() {
                 //...
+                Intent s = new Intent(Sex.this,MainActivity.class);
+                startActivity(s);
             }
         });
   
