42b4e3eb81bb77b6d0b0151aab920723b012948b
mahmoudyousuf <34246285+mahmoudyousuf@users.noreply.github.com>
2018-03-19 15:30:40 +0200

    Permissions

--- app/src/main/java/com/example/application/disabledv01/Dashboard.java
+++ app/src/main/java/com/example/application/disabledv01/Dashboard.java
@@ -1,6 +1,8 @@
 package com.example.application.disabledv01;
 
+import android.Manifest;
 import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,10 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        ActivityCompat.requestPermissions(Dashboard.this, new String[]{Manifest.permission.CALL_PHONE},1);
+
+
+        ActivityCompat.requestPermissions(Dashboard.this, new String[]{Manifest.permission.INTERNET},1);
     }
 
     public void battery(View v){
